4009bbbfd260340c8f4cad9a09ad21a905e67366
hbarrons <hbarrons@gmail.com>
2022-07-30 10:48:45 -0400

    update getPlayers request to return only non-deleted players

--- Data/DataService.cs
+++ Data/DataService.cs
@@ -16,14 +16,25 @@ namespace LionsDemo.Data
         public IEnumerable<Player> GetPlayers()
         {
             var players = Fakes.PlayerCollection;
-            return players;
+            Console.WriteLine("test");
+            Console.Write("all: ", players);
+            Console.Write("not deleted: ", players.Where(p => p.Deleted == false));
+            
+            return players.Where(p => p.Deleted == false);
         }
 
         // OBJECTIVE: Right now, I only return players who exactly match a players last name, and am case sensitive.
         // Make it so I return any player whose first or last name contains the searched string, non case sensitive.
         public Player GetPlayerByName(string playerName)
         {
+            //Console.WriteLine(playerName);
             var players = Fakes.PlayerCollection;
+            ////declaring a size here, but need to validate max # of players with same name?
+            //string[] searchedPlayers = new string[10];
+            //for (int i=0; i<players.Length; i++)
+            //{
+
+            //}
             return players.FirstOrDefault(p => p.LastName == playerName);
         }
 
